d5482e245be689f21ca4ab0790161c68f915dc5f
zjloveli1314 <114325781+zjloveli1314@users.noreply.github.com>
2022-09-25 13:07:38 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -67,7 +67,7 @@ const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: '',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: '-ZVlOW_xFxDt8oAnu1SgE3p4RREacjl51cry56L3eB0',
       // 所在省份
       province: '安徽',
       // 所在城市
